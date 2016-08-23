f5a73a92231f20706bcef5b92bd8b5b57e26c4a3
GeorgOfenbeck <ofenbeck@inf.ethz.ch>
2016-08-23 20:10:13 +0200

    forgot to add the ops

---
+++ src/main/scala/lms/ops/VectorOps.scala
@@ -0,0 +1,15 @@
+package scala.lms
+package ops
+
+import java.io.PrintWriter
+import org.scala_lang.virtualized.SourceContext
+import scala.lms.util._
+
+trait VectorOps extends Base with OverloadHack {
+
+  object NewVector {
+
+  }
+
+
+}
