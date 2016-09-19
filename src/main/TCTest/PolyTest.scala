38a3580dd706880ad967663e174acf7403ade2f4
GeorgOfenbeck <ofenbeck@inf.ethz.ch>
2016-09-19 14:54:20 +0200

    intermediate commit

--- src/main/TCTest/PolyTest.scala
+++ src/main/TCTest/PolyTest.scala
@@ -12,12 +12,7 @@ import scala.lms.internal._
 import scala.lms.ops._
 import scala.lms.targets.graphviz.GraphVizExport
 import scala.lms.targets.scalalike._
-import scala.spores._
-import scala.pickling._
-import scala.pickling.Defaults._
-import scala.pickling._
-import scala.pickling.json._
-import SporePicklers._
+
 
 class DSL2 extends BaseExp with FunctionsExp with BooleanOpsExpOpt with IfThenElsePureExp with PurePrimitiveOpsExp  with VectorOpsExp with OrderingOpsExp with RangeOpsExp with ImplicitOpsExp with ScalaCompile  {
   self =>
@@ -29,45 +24,149 @@ class DSL2 extends BaseExp with FunctionsExp with BooleanOpsExpOpt with IfThenEl
   }
 
 
-
-  trait IRep[T[_]]
-
-  implicit object isRep extends IRep[Rep]
-  implicit object noRep extends IRep[NoRep]
-
-  trait isStatic
-  trait isDynamic
-  //class Header[RA[_],RB[_],A,B](a: RA[A], b: RB[B])(implicit val eva: IRep[RA], val evb: IRep[RB])
-  class Header[RA[_],RB[_]](a: RA[Int], b: RB[Int])(implicit val eva: IRep[RA], val evb: IRep[RB])
-  class Mix[RA[_],RB[_]](a: RA[Int], b: RB[Int]) extends Header[RA,RB](a,b) with isStatic with isDynamic
-
-  object Mix{
-    def apply[RA[_],RB[_]](hs: Header[RA,RB] with isStatic, hd: Header[RA,RB] with isDynamic): Mix[RA,RB] = {
-      ???
+  abstract class NumericOps[T:Manifest] {	//inspired by Numeric
+    lazy val m = implicitly[Manifest[T]]
+    def plus       (x: T, y: T) : T
+    def minus      (x: T, y: T) : T
+    def times      (x: T, y: T) : T
+    class Ops(lhs: T) {
+      def +(rhs: T) = plus (lhs, rhs)
+      def -(rhs: T) = minus(lhs, rhs)
+      def *(rhs: T) = times(lhs, rhs)
     }
+    def mkNumericOps(lhs: T): Ops = new Ops(lhs)
+  }
+
+  protected abstract class NumericRepOps[T:Manifest:Numeric] extends NumericOps[Rep[T]]  {
+    def plus  (x : Rep[T], y: Rep[T]) = ??? ///numeric_plus [T](x, y)
+    def minus (x : Rep[T], y: Rep[T]) = ??? //numeric_minus[T](x, y)
+    def times (x : Rep[T], y: Rep[T]) = ??? //numeric_times[T](x, y)
+  }
+  implicit object IntRep extends NumericRepOps[Int]
+
+
+  protected abstract class NumericNoRepOps[T:Numeric:Manifest] extends NumericOps[T]  {
+    def plus  (x : T, y: T) = implicitly[Numeric[T]].plus (x, y)
+    def minus (x : T, y: T) = implicitly[Numeric[T]].minus(x, y)
+    def times (x : T, y: T) = implicitly[Numeric[T]].times(x, y)
+  }
+  implicit object IntNoRep    extends NumericNoRepOps[Int]
+  implicit object LongNoRep   extends NumericNoRepOps[Float]
+  implicit object FloatNoRep  extends NumericNoRepOps[Float]
+  implicit object DoubleNoRep extends NumericNoRepOps[Double]
+
+  trait IRep[T[_]] {
+    def isRep(): Boolean
+    def getRep[A](x: T[A]): Option[Rep[A]]
+    def getNoRep[A](x: T[A]): Option[A]
+    def fresh[A: TypeRep](): Vector[Rep[_]]
+    def fetch[A: TypeRep](x: Vector[Rep[_]]): (Vector[Rep[_]],Option[T[A]])
+    //def push[A: TypeRep](x: T[A]): Vector[Rep[_]]
+  }
+  implicit object isRep extends IRep[Rep] {
+    val isRep = true
+    def getRep[A](x: Rep[A]): Some[Rep[A]] = Some(x)
+    def getNoRep[A](x: Rep[A]): Option[A] = None
+    def fresh[A: TypeRep](): Vector[Rep[_]] = Vector(Arg[A])
+    def fetch[A: TypeRep](x: Vector[Rep[_]]): (Vector[Rep[_]],Some[Rep[A]]) = (x.tail,Some(x.head.asInstanceOf[Rep[A]]))
+    //def push[A: TypeRep](x: Rep[A]): Vector[Rep[_]] = Vector(x)
+  }
+  implicit object noRep extends IRep[NoRep] {
+    val isRep = false
+    def getRep[A](x: NoRep[A]): Option[Rep[A]] = None
+    def getNoRep[A](x: NoRep[A]): Some[A] = Some(x)
+    def fresh[A: TypeRep](): Vector[Rep[_]] = Vector.empty
+    def fetch[A: TypeRep](x: Vector[Rep[_]]): (Vector[Rep[_]],Option[NoRep[A]]) = (x,None)
+    //def push[A: TypeRep](x: NoRep[A]): Vector[Rep[_]] = Vector.empty
   }
 
 
-  def genf[RA[_],RB[_]](hs: Header[RA,RB] with isStatic)(implicit eva: IRep[RA], evb: IRep[RB]):
-  StagedFunction[Header[RA,RB] with isDynamic, Rep[Int]]= {
 
-    val expdyn: ExposeRep[Header[RA,RB] with isDynamic] = ???
-    val exprepint = exposeRepFromRep[Int]
 
-    def stageme(hd: Header[RA,RB] with isDynamic): Rep[Int] = {
-      val mix: Mix[RA,RB] = Mix.apply[RA,RB](hs,hd)
 
-      ???
+  //class Header[RA[_],RB[_],A,B](a: RA[A], b: RB[B])(implicit val eva: IRep[RA], val evb: IRep[RB])
+  abstract class Header[RA[_],RB[_]](a: RA[Int], b: RB[Int])(implicit eva: IRep[RA], evb: IRep[RB])
+  //access always together with static! need the - only needs to set
+  class DynHeader[RA[_],RB[_]]( a: RA[Int], b: RB[Int], val eva: IRep[RA], val evb: IRep[RB]) extends Header(a,b)(eva,evb){
+    def a(): Option[RA[Int]] = if(eva.isRep()) Some(a) else None
+    def b(): Option[RB[Int]] = if(evb.isRep()) Some(b) else None
+  }
+  //access also alone
+  class StatHeader[RA[_],RB[_]]( a: RA[Int], b: RB[Int], val eva: IRep[RA], val evb: IRep[RB]) extends Header(a,b)(eva,evb) {
+    def a(): Option[RA[Int]] = if(!eva.isRep()) Some(a) else None
+    def b(): Option[RB[Int]] = if(!evb.isRep()) Some(b) else None
+  }
+  class Mix[RA[_],RB[_]](val a: RA[Int], val b: RB[Int], val eva: IRep[RA], val evb: IRep[RB]) extends Header[RA,RB](a,b)(eva, evb)
+
+  object Mix{
+    private def choose[A[_], T](a: Option[A[T]], b: Option[A[T]], ev: IRep[A]): A[T] = {
+      if (ev.isRep()) b.get else a.get
     }
 
-    val sf: StagedFunction[Header[RA,RB] with isDynamic, Rep[Int]] = doGlobalLambda(stageme,false)(expdyn,exprepint)
+    def apply[RA[_],RB[_]](hs: StatHeader[RA,RB], hd: DynHeader[RA,RB]): Mix[RA,RB] = {
+      val a: RA[Int] = choose(hs.a(),hd.a(), hd.eva)
+      val b: RB[Int] = choose(hs.b(),hd.b(), hd.evb)
+      new Mix(a,b, hd.eva, hd.evb)
+    }
+  }
+
+  implicit def exposeDynHeader[RA[_],RB[_]](stat: StatHeader[RA,RB]): ExposeRep[DynHeader[RA,RB]] = new ExposeRep[DynHeader[RA,RB]]() {
+      val freshExps: Unit => Vector[Exp[_]] = (u: Unit) => stat.eva.fresh() ++ stat.evb.fresh()
+      val vec2t: Vector[Exp[_]] => DynHeader[RA,RB] = (in: Vector[Exp[_]]) => {
+        def help[T[_],A: TypeRep](in: Vector[Rep[_]], statele: Option[T[A]], ev: IRep[T]): (Vector[Rep[_]],T[A]) = {
+          val (vecafter, ele) = ev.fetch[A](in)
+          val res: T[A] = ele.getOrElse(statele.get)
+          (vecafter,res)
+        }
+        val (aa, a) = help(in,stat.a(),stat.eva)
+        val (ab, b) = help(in,stat.b(),stat.evb)
+        new DynHeader[RA,RB](a,b,stat.eva,stat.evb)
+      }
+      val t2vec: DynHeader[RA,RB] => Vector[Exp[_]] = (in: DynHeader[RA,RB]) => {
+        def help[T[_],A](ele: Option[T[A]], ev: IRep[T]): Vector[Rep[_]] = {
+          if (ev.isRep()) Vector(ev.getRep(ele.get).get) else Vector.empty
+        }
+        help(in.a, in.eva) ++ help(in.b, in.evb)
+      }
+    }
+
+
+
+  def genf[RA[_],RB[_]](hs: StatHeader[RA,RB])
+                       (implicit eva: IRep[RA],
+                        evb: IRep[RB],
+                        gnuma: NumericOps[RA[Int]],
+                        gnumb: NumericOps[RB[Int]]
+                       ):
+  StagedFunction[DynHeader[RA,RB], Rep[Int]]= {
+    val expdyn: ExposeRep[DynHeader[RA,RB]] = exposeDynHeader(hs)
+    val exprepint = exposeRepFromRep[Int]
+
+    def stageme(hd: DynHeader[RA,RB]): Rep[Int] = {
+      /*implicit def infixNumericOps[T[_]](x: T[Int])(implicit num: GNumeric[T]): GNumeric[T]#Ops = new num.Ops(x)
+      import gnuma._
+      import gnumb._*/
+
+
+      val mix: Mix[RA,RB] = Mix.apply[RA,RB](hs,hd)
+      val a = gnuma.times(mix.a,mix.a)
+      val b = gnumb.times(mix.b,mix.b)
+
+      // split again here!
+
+      val sf = genf()
+
+
+    }
+
+    val sf: StagedFunction[DynHeader[RA,RB], Rep[Int]] = doGlobalLambda(stageme,false)(expdyn,exprepint)
     sf
 
 
   }
 
   def start(i: Rep[Int]):Rep[Int] = {
-    val s: Header[Rep,NoRep] with isStatic = ???
+    val s: StatHeader[Rep,NoRep]  = ???
     val sf = genf(s)
     println(sf)
     i
